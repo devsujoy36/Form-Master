147297634cbd42ad02e634239e3fd8fcb8a4526d
unknown <sujoydaspc2023@gmail.com>
2024-12-24 17:38:03 +0600

    adding moneyContest

--- src/Component/Family/Aunty/Aunty.jsx
+++ src/Component/Family/Aunty/Aunty.jsx
@@ -1,13 +1,24 @@
+import { useContext } from "react"
 import Child from "../Dad/Child"
+import { MoneyContext } from "../Grandpa/Grandpa"
 
 const Aunty = () => {
+
+  const btnStyle = "text-white font-semibold bg-emerald-600 hover:bg-emerald-500 active:scale-95 px-4 py-2 rounded-lg transition-all active:-skew-x-6"
+
+  const [money, setMoney] = useContext(MoneyContext)
+
   return (
     <div className="granpa">
       <h1 className="text-center text-xl font-bold">Aunty</h1>
+      <p className="main font-bold text-center">Money: {money}</p>
       <section className="flex">
         <Child name={"Anay"} />
         <Child name={"Mistu"} />
       </section>
+      <div className="flex justify-center items-center">
+        <button onClick={() => setMoney(money + 1000)} className={btnStyle}>Add 1000Tk</button>
+      </div>
     </div>
   )
 }
--- src/Component/Family/Grandpa/Grandpa.jsx
+++ src/Component/Family/Grandpa/Grandpa.jsx
@@ -1,21 +1,30 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import Aunty from "../Aunty/Aunty"
 import Dad from "../Dad/Dad"
 import Uncle from "../Uncle/Uncle"
 import './Grandpa.css'
 
-export const AssetContext = createContext('Gold')
+export const AssetContext = createContext();
+export const MoneyContext = createContext()
 
 const Grandpa = () => {
+const [money, setMoney] = useState(null)
+
   return (
     <div className="main">
-      <h1 className="text-center font-bold text-2xl">Grandpa</h1>
+      <div className="text-center font-bold text-2xl">
+      <h1>Grandpa</h1>
+      <h1>Bank Balance: {money}</h1>
+
+      </div>
       <section className="grandpa flex ">
-        <AssetContext.Provider value="Gold">
-          <Dad></Dad>
-          <Uncle></Uncle>
-          <Aunty></Aunty>
-        </AssetContext.Provider>
+        <MoneyContext.Provider value={[money, setMoney]}>
+          <AssetContext.Provider value="Gold">
+            <Dad></Dad>
+            <Uncle></Uncle>
+            <Aunty></Aunty>
+          </AssetContext.Provider>
+        </MoneyContext.Provider>
       </section>
     </div>
   )
